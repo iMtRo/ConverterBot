a142abd356c6c7a4d5c8a85e525c059e07d28733
Maxmudov Ilyos <97552255+iMtRo@users.noreply.github.com>
2025-08-07 05:00:24 +0500

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,6 @@
+import os
+
+API_ID = int(os.getenv("24280776"))
+API_HASH = os.getenv("23a4634941a7687050132a75f3f1e20b")
+BOT_TOKEN = os.getenv("8405572224:AAFEUq7d-3xXixStAHrRIMQ7-4oM_DYDzAY")
+OWNER_ID = int(os.getenv("5556896112"))
